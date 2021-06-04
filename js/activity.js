31e5c3ebbd963247d8513369f2a55aad181e1f75
Tatyana Pavlova <t.pavlova.spb@yandex.ru>
2021-06-04 03:43:45 +0300

    added menu buttons, started creating function to add divs dynamically

--- js/activity.js
+++ js/activity.js
@@ -1,3 +1,5 @@
+// drag'n'drop elements
+
 let draggableElements = document.getElementsByClassName("element");
 
 for (let i = 0; i < draggableElements.length; i++) {
@@ -37,14 +39,21 @@ function dragElement(elem, i) {
         elem.style.left = (elem.offsetLeft - pos1) + "px";
 
         var line1 = document.getElementById("line1");
+        var line2 = document.getElementById("line2");
         
         if (i == 0) {
             line1.setAttribute("x1", e.clientX);
             line1.setAttribute("y1", e.clientY);
         }
-        else {
+        else if (i == 1) {
             line1.setAttribute("x2", e.clientX);
             line1.setAttribute("y2", e.clientY);
+            line2.setAttribute("x1", e.clientX);
+            line2.setAttribute("y1", e.clientY);
+        }
+        else {
+            line2.setAttribute("x2", e.clientX);
+            line2.setAttribute("y2", e.clientY);
         }
         
     }
@@ -53,9 +62,47 @@ function dragElement(elem, i) {
         document.onmouseup = null;
         document.onmousemove = null;
     }
-
-    
-    
 }
 
+// new element
 
+function newElement() {
+    // alert("Hi");
+
+    var ok = true;
+    var i = 0;
+
+    if (ok === true) {
+        // create element
+        var div = document.createElement("div");
+        // define className
+        div.className = "element";
+        // define id
+        for (i = 0; i < draggableElements.length; i++) {
+            i++;
+        }
+        div.id = "div" + i;
+
+        // define positions
+        div.style.left = "50px";
+        div.style.top = "50px";
+
+        document.getElementsByTagName('body')[0].appendChild(div);
+
+        // add picture and title
+
+        // create element
+        var elem = document.createElement("div");
+        // define className
+        elem.className = "element__picture";
+        // define id
+        elem.id = "elem" + i;
+
+        document.getElementById(div.id).appendChild(elem);
+    }
+
+    // calling drag function again after creating new div
+    for (let i = 0; i < draggableElements.length; i++) {
+        dragElement(draggableElements[i], i);
+    }
+}
