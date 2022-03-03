c86414d9c25c317d59e4216c6364407c521ad057
Pavlova Tatyana <tpavlova.sm@yandex.ru>
2022-03-04 02:14:27 +0300

    saves the current scale value and uses it after page reload

--- js/activity.js
+++ js/activity.js
@@ -96,23 +96,49 @@ let lineNumber = 0;
 
 // zoom
 
-let scale = 1;
+setZoomValue();
 
 function zoom(name) {
+
+    let scale = Number(getZoomValue());
+    
     // 1.0 is default value
     if (name == 'in') {
         // if zoom is not max yet
         if (scale < 1) {
-            scale = scale + 0.1;
-            container.style.transform = "scale(" + scale + ")";
+            scale = (scale + 0.1).toFixed(1);
         }
     }
     else if (name == 'out') {
         if (scale > 0.6) {
-            scale = scale - 0.1;
-            container.style.transform = "scale(" + scale + ")";
+            scale = (scale - 0.1).toFixed(1);
         }
     }
+
+    container.style.transform = "scale(" + scale + ")";
+
+    localStorage.setItem("zoom", scale);
+}
+
+function setZoomValue() {
+
+    let scale = getZoomValue();
+
+    container.style.transform = "scale(" + scale + ")";
+}
+
+function getZoomValue() {
+
+    let scale = "";
+
+    if (isNaN(localStorage.getItem("zoom")) || (localStorage.getItem("zoom") == null)) {
+        scale = localStorage.setItem("zoom", 1);
+    }
+    else {
+        scale = localStorage.getItem("zoom");
+    }
+
+    return scale;
 }
 
 // edit item
